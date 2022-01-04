6ddc9696e0343534104d15cd22a1240f6bfc385b
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2022-01-05 00:16:07 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Inheritance_Concept
         static void Main(string[] args)
         {
             PartTimeEmployee ep = new PartTimeEmployee("amine", "amine@gmail.com", 4, 30F);
-
+            FullTimeEmployee ep_ = new FullTimeEmployee("khalil", "khalil@gmail.com", 12, 5000F);
+            Employee ep__ = new PartTimeEmployee("emma", "emma@gmail.com", 7, 20000F);
         }
     }
 }
